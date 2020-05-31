81a0c8c8fac02048b8c06b6599725df9603658e8
Abel Kalum <chumbabel@gmail.com>
2020-05-31 14:29:00 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require 'pry'
 
 class App < Sinatra::Base
+
+  get '/' do
+    erb :user_input
+  end
+
+  post '/piglatinize' do
+    user_word = params["user_phrase"]
+    word = PigLatinizer.new
+    @changed_text = word.to_pig_latin(user_word)
+    
+    erb :piglatinize
+  end
 end
