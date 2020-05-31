ec8112e2ffbaacbb6c903f79873f3b500922d74b
Abel Kalum <chumbabel@gmail.com>
2020-05-31 14:32:54 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,17 +1,17 @@
 require_relative 'config/environment'
-require 'pry'
 
 class App < Sinatra::Base
 
-  get '/' do
-    erb :user_input
-  end
+get '/' do
+  erb :user_input
+end 
 
-  post '/piglatinize' do
-    user_word = params["user_phrase"]
-    word = PigLatinizer.new
-    @changed_text = word.to_pig_latin(user_word)
-    
-    erb :piglatinize
-  end
+post '/piglatinize' do 
+
+  ring = PigLatinizer.new
+  @string = ring.pig(params[:user_phrase])  
+  erb :piglatinize
+
+end 
+
 end
