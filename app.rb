9f64c78d78b841cac859b3093da2a55c3439af38
Abel Kalum <chumbabel@gmail.com>
2020-05-31 14:33:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,16 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-get '/' do
-  erb :user_input
-end 
+  get '/' do
+    erb :user_input
+  end 
 
-post '/piglatinize' do 
+  post '/piglatinize' do 
 
-  ring = PigLatinizer.new
-  @string = ring.pig(params[:user_phrase])  
-  erb :piglatinize
+    ring = PigLatinizer.new
+    @string = ring.pig(params[:user_phrase])  
+    erb :piglatinize
 
-end 
+  end 
 
 end
